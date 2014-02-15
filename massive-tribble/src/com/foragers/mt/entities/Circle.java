d86fc62e960c37766ed030697aa8f1c3e33ca622
dam4256 <arondeldamien@orange.fr>
2014-02-15 16:26:25 +0100

    #1 Ajout de la fonctionnalité de dessin d'un cercle

---
+++ massive-tribble/src/com/foragers/mt/entities/Circle.java
@@ -0,0 +1,57 @@
+package com.foragers.mt.entities;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class Circle {
+
+	private Texture texture;
+	private Pixmap pixmap;
+	private Vector2 position;
+
+	private int diameterMin;
+	private int diameterMax;
+	private int lifetime;
+
+	public Circle(Color color, int x, int y, int diameterMin, int diameterMax,
+			int lifetime) {
+		this.diameterMin = diameterMin;
+		this.diameterMax = diameterMax;
+		this.lifetime = lifetime;
+
+		make(color, x, y);
+	}
+
+	public void render(SpriteBatch batcher) {
+		batcher.draw(texture, position.x, position.y);
+	}
+
+	public void dispose() {
+		texture.dispose();
+		pixmap.dispose();
+	}
+
+	private void make(Color color, int x, int y) {
+		// Get the largest power of two for the diameter.
+		int size = isPowerOfTwo(diameterMax) ? diameterMax : (int) Math.pow(2,
+				Math.ceil(Math.log(diameterMax) / Math.log(2)));
+
+		pixmap = new Pixmap(size, size, Pixmap.Format.RGBA8888);
+		pixmap.setColor(color);
+		pixmap.drawCircle(size / 2, size / 2, diameterMax / 2);
+		
+		texture = new Texture(pixmap);
+		texture.draw(pixmap, 0, 0);
+		texture.bind();
+		
+		position = new Vector2(x - size / 2, y - size / 2);
+	}
+
+	private boolean isPowerOfTwo(int x) {
+		return x > 0 && (x & (x - 1)) == 0;
+	}
+
+}
--- massive-tribble/src/com/foragers/mt/screens/GameScreen.java
+++ massive-tribble/src/com/foragers/mt/screens/GameScreen.java
@@ -1,35 +1,66 @@
 package com.foragers.mt.screens;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.foragers.mt.entities.Circle;
 
 public class GameScreen implements Screen {
 
-	SpriteBatch batcher;
-	
+	private int width = 800;
+	private int height = 480;
+
+	private SpriteBatch batcher = new SpriteBatch();
+	private List<Circle> circles = new ArrayList<Circle>();
+
 	public GameScreen() {
-		batcher = new SpriteBatch();
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+
+		for (int i = 0; i < 1; i++) {
+			circles.add(new Circle(Color.YELLOW, width / 2, height / 2, 64, 64, 1000));
+		}
 	}
-	
-	@Override
-	public void render(float delta) { }
 
 	@Override
-	public void resize(int width, int height) { }
+	public void render(float delta) {
+		batcher.begin();
+		for (Circle circle : circles) {
+			circle.render(batcher);
+		}
+		batcher.end();
+	}
 
 	@Override
-	public void show() { }
+	public void resize(int width, int height) {
+	}
 
 	@Override
-	public void hide() { }
+	public void show() {
+	}
 
 	@Override
-	public void pause() { }
+	public void hide() {
+	}
 
 	@Override
-	public void resume() { }
+	public void pause() {
+	}
 
 	@Override
-	public void dispose() { }
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		for (Circle circle : circles) {
+			circle.dispose();
+		}
+		batcher.dispose();
+	}
 
 }
