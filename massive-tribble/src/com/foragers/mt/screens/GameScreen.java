16fb6f64be3fec2ecaa3fe45627b2ce5a640d2f7
dam4256 <arondeldamien@orange.fr>
2014-02-15 15:01:11 +0100

    #12 Mise en place du gestionnaire d'écrans

---
+++ massive-tribble/src/com/foragers/mt/screens/GameScreen.java
@@ -0,0 +1,35 @@
+package com.foragers.mt.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen implements Screen {
+
+	SpriteBatch batcher;
+	
+	public GameScreen() {
+		batcher = new SpriteBatch();
+	}
+	
+	@Override
+	public void render(float delta) { }
+
+	@Override
+	public void resize(int width, int height) { }
+
+	@Override
+	public void show() { }
+
+	@Override
+	public void hide() { }
+
+	@Override
+	public void pause() { }
+
+	@Override
+	public void resume() { }
+
+	@Override
+	public void dispose() { }
+
+}
