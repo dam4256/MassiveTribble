404711a1c53e01608547efc5309faec3fd8a431c
dam4256 <arondeldamien@orange.fr>
2014-02-19 21:36:32 +0100

    #14 Réorganisation du code

--- massive-tribble/src/com/foragers/mt/entities/CircleFactory.java
+++ massive-tribble/src/com/foragers/mt/entities/CircleFactory.java
@@ -12,8 +12,8 @@ public class CircleFactory {
 	private static final int ATTEMPT_BEFORE_DROP = 500;
 	private int _AttemptMarginCpt = 0;
 
-	private int nbTotal;
-	private int nbCurrent=0;
+	private int nb;
+	private int nbCurrent = 0;
 	private int lifeTime = 500;
 	private int margin = 10;
 	private float initialMargin = 10f;
@@ -21,161 +21,158 @@ public class CircleFactory {
 	private int width;
 	private int radiusMin;
 	private int radiusMax;
-	private List<Vector2> positList = new ArrayList<Vector2>();
+	
+	private List<Vector2> positions = new ArrayList<Vector2>();
 
 	public CircleFactory(int nb, int height, int width, int radiusMin, int radiusMax) {
-		this.nbTotal = nb;
+		this.nb = nb;
 		this.height = height;
 		this.width = width;
 		this.radiusMin = radiusMin;
 		this.radiusMax = radiusMax;
-		computePositions(); //compute the future position
+		
+		computePositions();
+	}
+	
+	public Actor makeCircle(int order, Color color) {
+		float x, y;
+		
+		if (nbCurrent > nb) {
+			x = positions.get(0).x;
+			y = positions.get(0).y;
+		} else {
+			x = positions.get(nbCurrent).x;
+			y = positions.get(nbCurrent).y;
+		}
+		
+		nbCurrent++;
+		
+		return new Circle(order, (int) x, (int) y, color, 2 * radiusMin, 2 * radiusMax, lifeTime);
 	}
 
+	public void setLifeTime(int lifetime) {
+		lifeTime = lifetime;
+	}
+
+	public void setPixMargin(int margin) {
+		this.margin = margin;
+		initialMargin = margin;
+	}
 
 	private void computePositions() {
 		int attempt = 0;
 		initPosition();
-		//while overlap
-		while(attempt < ATTEMPT_BEFORE_DROP && !rulesRespects()){
+		while (attempt < ATTEMPT_BEFORE_DROP && !rulesRespects()) {
 			movePositions();
 			computeDecreaseMargin(attempt);
 			attempt++;
 		}
 	}
 
-
-	/**All margin% attempt step --> the margin deacrease.<br/>
-	 * Example : initial margin = 10 --> all 10%attempt --> current margin --
-	 * @param attempt 
-	 */
-	private void computeDecreaseMargin(final int attempt) {
-		int percentPerformAttempt = (attempt - _AttemptMarginCpt);
-		float percentLimit = (initialMargin/100f) * ATTEMPT_BEFORE_DROP;
-		if((percentPerformAttempt  )> percentLimit){
-			this.margin--;
-			this._AttemptMarginCpt = attempt;
+	private void initPosition() {
+		int indexPost = 0, x, y;
+		while (indexPost < nb){
+			 x = radiusMax + 1 + (int) (Math.random() * (width - 2 * (radiusMax + margin) - 1));
+			 y = radiusMax + 1 + (int) (Math.random() * (height - 2 * (radiusMax + margin) - 1));
+			positions.add(new Vector2(x, y));
+			indexPost++;
 		}
 	}
+	
+	private boolean rulesRespects() {
+		boolean result = true;
 
-
-/*
- * Move a little the overlap circles
- * */
+		for (int i = 0; i < nb - 1; i++) {
+			for (int j = i + 1; j < nb; j++) {
+				result = circlesOverlaps(positions.get(i), positions.get(j));
+				if (result) {
+					break;
+				}
+			}
+			
+			if (result) {
+				break;
+			}
+		}
+		
+		return !result;
+	}
+	
 	private void movePositions() {
 		boolean result = true;
-		for(int i =0; i < this.nbTotal -1 ; i++){
-			for(int j=i+1; j < this.nbTotal ; j++ ){
-				Vector2 v1 = this.positList.get(i);
-				Vector2 v2 = this.positList.get(j);
-				result = circlesOverlaps(v1 ,this.positList.get(j));
-				int offSet = 1+ (int) (Math.random() *5);
-				if(result){
-					if(v1.x > v2.x && v1.y < v2.y){
+		
+		for (int i = 0; i < nb - 1; i++) {
+			for (int j = i + 1; j < nb; j++) {
+				Vector2 v1 = positions.get(i);
+				Vector2 v2 = positions.get(j);
+				result = circlesOverlaps(v1, positions.get(j));
+				int offSet = 1 + (int) (Math.random() * 5);
+				
+				if (result) {
+					if (v1.x > v2.x && v1.y < v2.y) {
 						v1.x += offSet;
 						v1.y -= offSet;
 						v2.x -= offSet;
 						v2.y += offSet;
-					}else{
+					} else {
 						v1.x += offSet;
 						v1.y += offSet;
 						v2.x -= offSet;
 						v2.y -= offSet;
 					}
-					if(v1.x< this.radiusMax){
-						v1.x = this.radiusMax +1;
-					}
-					if(v2.x< this.radiusMax){
-						v2.x = this.radiusMax +1;
-					}
-					if(v1.y< this.radiusMax){
-						v1.y = this.radiusMax +1;
-					}
-					if(v2.y< this.radiusMax){
-						v2.y = this.radiusMax +1;
+					
+					if (v1.x < radiusMax) {
+						v1.x = radiusMax + 1;
 					}
 					
-					if(v1.x+this.radiusMax> this.width){
-						v1.x = this.width - this.radiusMax - 1;
-					}
-					if(v2.x+this.radiusMax> this.width){
-						v2.x = this.width - this.radiusMax - 1;
+					if (v2.x < radiusMax) {
+						v2.x = radiusMax + 1;
 					}
 					
-					if(v1.y+this.radiusMax> this.height){
-						v1.y = this.height - this.radiusMax - 1;
+					if (v1.y < radiusMax) {
+						v1.y = radiusMax + 1;
 					}
-					if(v2.y+this.radiusMax> this.height){
-						v2.y = this.height - this.radiusMax -1 ;
+					
+					if (v2.y < radiusMax) {
+						v2.y = radiusMax + 1;
+					}
+
+					if (v1.x + radiusMax > width) {
+						v1.x = width - radiusMax - 1;
+					}
+					
+					if (v2.x + radiusMax > width) {
+						v2.x = width - radiusMax - 1;
+					}
+
+					if (v1.y + radiusMax > height) {
+						v1.y = height - radiusMax - 1;
+					}
+					
+					if (v2.y + radiusMax > height) {
+						v2.y = height - radiusMax - 1;
 					}
 				}
 			}
 		}
 	}
 
-
-	/**
-	 * the rules is respects ffi :
-	 * [-the circles are contains into the screen] --> verify by movePositions();
-	 * -the circles don't touch them
-	 * */
-	private boolean rulesRespects() {
-		boolean result = true;
-		for(int i =0; i < this.nbTotal -1 ; i++){
-			for(int j=i+1; j < this.nbTotal ; j++ ){
-				result = circlesOverlaps(this.positList.get(i),this.positList.get(j));
-				if(result) break;
-			}
-			if(result) break;
+	private void computeDecreaseMargin(int attempt) {
+		int percentPerformAttempt = (attempt - _AttemptMarginCpt);
+		float percentLimit = (initialMargin / 100f) * ATTEMPT_BEFORE_DROP;
+		
+		if (percentPerformAttempt > percentLimit) {
+			margin--;
+			_AttemptMarginCpt = attempt;
 		}
-		return !result;
 	}
 
 	private boolean circlesOverlaps(Vector2 vector1, Vector2 vector2) {
 		float dx = vector1.x - vector2.x;
 		float dy = vector1.y - vector2.y;
-		float distance = dx * dx + dy * dy;
-		float radiusSum = 2*this.radiusMin  + this.margin;
-		return distance < radiusSum * radiusSum;
+		float radiusSum = 2 * radiusMin + margin;
+		
+		return dx * dx + dy * dy < radiusSum * radiusSum;
 	}
 
-	/**
-	 * initialize with random position
-	 * */
-	private void initPosition() {
-		int indexPost = 0, x,y ;
-		while(indexPost < nbTotal){
-			 x = radiusMax + 1 + (int) (Math.random() * (width - 2 * (radiusMax+margin) - 1));
-			 y = radiusMax + 1 + (int) (Math.random() * (height - 2 * (radiusMax+margin) - 1));
-			this.positList.add(new Vector2(x, y));
-			indexPost++;
-		}
-	}
-
-
-
-	public Actor makeCircle(int order, Color color) {
-
-		float x,y;
-		if(this.nbCurrent>this.nbTotal){
-			//WARNING !!
-			x = positList.get(0).x;
-			y = positList.get(0).y;
-		}else{
-			x = positList.get(nbCurrent).x;
-			y = positList.get(nbCurrent).y;
-		}
-		Actor result = new Circle(order, (int)x, (int) y, color, 2 * radiusMin, 2 *radiusMax , lifeTime);
-		this.nbCurrent++;
-		return result;
-	}
-
-	public void setLifeTime(int lifetime) {
-		this.lifeTime = lifetime;
-	}
-
-	public void setPixMargin(int margin) {
-		this.margin = margin;
-		this.initialMargin = margin;
-	}
 }
--- massive-tribble/src/com/foragers/mt/screens/GameScreen.java
+++ massive-tribble/src/com/foragers/mt/screens/GameScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.foragers.mt.Media;
 import com.foragers.mt.entities.Circle.Color;
